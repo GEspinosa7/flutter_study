74b31b2385151d1e8e3c76c632be51e625acdd85
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-09-14 13:43:07 -0300

    separate the constants

---
+++ bmi-calculator/lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const kbottomContainerHeight = 80.0;
+const Color kbottomContainerBgColor = Color(0xFFEB1555);
+const Color kinactiveItemCardColor = Color(0xFF8D8E98);
+const Color kactiveItemCardColor = Colors.white;
+const Color kinactiveBgCardColor = Color(0xFF111328);
+const Color kactiveBgCardColor = Color(0xFF1D1E33);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 25,
+  color: Color(0xFF8D8E98),
+  fontFamily: 'Pacifico',
+);
+
+const kHeightTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
