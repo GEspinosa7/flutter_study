f09f81280437eaa4d876614f6f932a76dd79ada5
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-09-17 14:48:20 -0300

    reafct code

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 import '../services/location.dart';
 
 const apiKey = '76eb8dc50ae704ff7c7b74ac367ece96';
@@ -17,40 +16,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
     latitude = location.latitude;
     longitude = location.longitude;
 
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper netHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      double temp = decodedData['main']['temp'];
-
-      int condition = decodedData['weather'][0]['id'];
-
-      String city = decodedData['name'];
-
-      print(temp);
-      print(condition);
-      print(city);
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = await netHelper.getData();
   }
 
   @override
