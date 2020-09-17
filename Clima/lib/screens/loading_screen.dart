34f21b00bc15955650b10c2ca43adb77616d636e
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-09-17 14:43:47 -0300

    get real time weather

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -4,29 +4,35 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../services/location.dart';
 
+const apiKey = '76eb8dc50ae704ff7c7b74ac367ece96';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   @override
   void initState() {
     super.initState();
     getLocation();
-    getData();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     if (response.statusCode == 200) {
       String data = response.body;
